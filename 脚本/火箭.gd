364bbfc2e1fc1e4091af0388416b80bf303a711e
GameCellsStudio <309607772@qq.com>
2024-06-28 00:01:21 +0800

    为游戏添加了背景音乐，死亡爆炸音效，添加了火箭爆炸后玩家禁用控制等功能

--- 脚本/火箭.gd
+++ 脚本/火箭.gd
@@ -1,20 +1,25 @@
 extends RigidBody3D
 
+@onready var 坠毁音效: AudioStreamPlayer = $"坠毁音效"
+
 ##移动时施加的垂直力大小
 @export_range(750.0,2500.0) var 推力 := 1000.0
 ##控制左右旋转的速度
 @export var 扭力 := 100.0
 
+var is_explosion = false
+
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("起飞"):
-		apply_central_force(basis.y * delta * 推力)
-	if Input.is_action_pressed("左旋转"):
-		apply_torque(Vector3.BACK * 扭力 * delta)
-	if Input.is_action_pressed("右旋转"):
-		apply_torque(-Vector3.BACK * 扭力 * delta)
+	if not is_explosion:
+		if Input.is_action_pressed("起飞"):
+			apply_central_force(basis.y * delta * 推力)
+		if Input.is_action_pressed("左旋转"):
+			apply_torque(Vector3.BACK * 扭力 * delta)
+		if Input.is_action_pressed("右旋转"):
+			apply_torque(-Vector3.BACK * 扭力 * delta)
 
 
 func _on_body_entered(body: Node) -> void:
@@ -24,8 +29,15 @@ func _on_body_entered(body: Node) -> void:
 		坠毁()
 
 func 坠毁() -> void:
+	if not 坠毁音效.playing:
+		坠毁音效.play()
+		
+	is_explosion = true
+	
 	print("KBOOM~火箭坠毁了！")
+	await get_tree().create_timer(1.5).timeout
 	call_deferred("延迟重载关卡")
+	
 
 func 完成关卡(下一关场景文件:String) -> void:
 	print("叮当！你已安全着陆！")
