1af553ec9a37ced557447f6bd5153ecd47d9f7b9
GameCellsStudio <309607772@qq.com>
2024-06-27 22:49:16 +0800

    添加了成功着陆后跳转到下一关的功能， 建立了第二关的简易场景， 在第二关中放置了一个障碍物

--- 脚本/火箭.gd
+++ 脚本/火箭.gd
@@ -19,7 +19,7 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node) -> void:
 	if "着陆台" in body.get_groups():
-		完成关卡()
+		完成关卡(body.下一关场景路径)
 	if "障碍物" in body.get_groups():
 		坠毁()
 
@@ -27,9 +27,13 @@ func 坠毁() -> void:
 	print("KBOOM~火箭坠毁了！")
 	call_deferred("延迟重载关卡")
 
-func 完成关卡() -> void:
+func 完成关卡(下一关场景文件:String) -> void:
 	print("叮当！你已安全着陆！")
-	get_tree().quit()
+	call_deferred("延迟加载关卡",下一关场景文件)
+	
 	
 func 延迟重载关卡() -> void:
 	get_tree().reload_current_scene()
+
+func 延迟加载关卡(下一关场景文件) -> void:
+	get_tree().change_scene_to_file(下一关场景文件)
