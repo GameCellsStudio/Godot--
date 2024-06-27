cd6b105079eb5218ae0d481ef001ce4c287626dc
GameCellsStudio <309607772@qq.com>
2024-06-27 22:17:31 +0800

    学习Godot3D的第一个项目-火箭着陆小游戏

---
+++ 脚本/火箭.gd
@@ -0,0 +1,35 @@
+extends RigidBody3D
+
+##移动时施加的垂直力大小
+@export_range(750.0,2500.0) var 推力 := 1000.0
+##控制左右旋转的速度
+@export var 扭力 := 100.0
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("起飞"):
+		apply_central_force(basis.y * delta * 推力)
+	if Input.is_action_pressed("左旋转"):
+		apply_torque(Vector3.BACK * 扭力 * delta)
+	if Input.is_action_pressed("右旋转"):
+		apply_torque(-Vector3.BACK * 扭力 * delta)
+
+
+func _on_body_entered(body: Node) -> void:
+	if "着陆台" in body.get_groups():
+		完成关卡()
+	if "障碍物" in body.get_groups():
+		坠毁()
+
+func 坠毁() -> void:
+	print("KBOOM~火箭坠毁了！")
+	call_deferred("延迟重载关卡")
+
+func 完成关卡() -> void:
+	print("叮当！你已安全着陆！")
+	get_tree().quit()
+	
+func 延迟重载关卡() -> void:
+	get_tree().reload_current_scene()
